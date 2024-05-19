9c865f95fb6b4a7ea54511071023c17ccf00cb28
Sirfak3 <166884016+Sirfak3@users.noreply.github.com>
2024-05-19 15:36:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,52 @@
-function nmr(number){
-    document.getElementById("tela").value += number;
+function nmr(nmr){
+    document.getElementById("tela").value += nmr; // Permite adicionar búmeros na tela.
 }
 function operador(op){
-    document.getElementById("tela").value += op;
+    document.getElementById("tela").value += op; // Permite adiocionar operadores na tela.
 }
-function clearTela(){
-    document.getElementById("tela").value = ''; // Limpa a tela
-    document.getElementById("historico").value = ''; // Limpa a tela superior
-} 
+function clearTela(){ // Limpa a tela e o histórico.
+    document.getElementById("tela").value = '';
+    document.getElementById("historico").value = '';
+}
 
+// Funções para memoria ▼.
+var memoria = document.getElementById("tela").value;
+memoria = memoria;
+
+function adcMemoria(){
+    memoria = document.getElementById("tela").value;
+}
+function chamarMemoria(){
+    document.getElementById("tela").value = memoria;
+}
+function clearMemoria(){
+    memoria = '';
+}
+
+// Função para deletar o ultimo número ▼.
+function deletar(){
+    const valores = document.getElementById("tela").value;
+    const valoresnew = valores.replace(/.$/, '');
+    document.getElementById("tela").value = valoresnew;
+}
+
+// Função para calcular ▼.
 function calculateResult(){
-    let conta = document.getElementById("tela").value;
+    let conta = document.getElementById("tela").value; 
     document.getElementById("historico").value = conta; // Imprime a expressão na caixa superior.
 
+    // Substitui os simbolos ▼.
     conta = conta.replace(/÷/g, '/');
     conta = conta.replace(/×/g, '*');
     conta = conta.replace(/,/g, '.');
+
+    conta = conta.replace(/^0+(?=[^,])/, ""); // Remove o 0(zero) da conta que inicie com o mesmo e não seja seguido de uma virgula.
     
-    conta = conta.replace(/^0+(?=[^,])/, "");
-
     try{
-        let result = eval(conta); // Calcula a expressão da tela. Retorna undefined se a expressão estiver vazia.
-
-        document.getElementById("tela").value = result; // Imprime na tela o resultado do calculo.
-
+        let result = eval(newconta); // Transforma o valor da tela em uma expressão númerica. Retorna undefined se a tela estiver vazia.
+        document.getElementById("tela").value = result; // Imprime o resultado da expressão na tela.
     }
-    catch (error){
-        document.getElementById("tela").value = 'Erro'; // Imprime na tela a mensagem erro se ouver algo que nao foi especificado no try.
+    catch(error){
+        document.getElementById("tela").value = 'Erro'; // Retorna a mensagem erro se houver algo não especificado no try.
     }
 }
